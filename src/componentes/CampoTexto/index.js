bcf6c93951c7aec5852adb00a946d0f0106925a8
Leonardo Souza <Leonardo23s.pt@gmail.com>
2023-08-23 10:14:39 +0100

    atualizando e melhorando organo com implementação de novas funções

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,15 @@
+import './campo-texto.css'
+
+const CampoTexto = ({ type = 'text', label, placeholder, valor, aoAlterado, obrigatorio = false }) => {
+    return (<div className={`campo campo-${type}`}>
+        <label>{label}</label>
+        <input 
+        type={type} 
+        value={valor} 
+        onChange={evento => aoAlterado(evento.target.value)} 
+        required={obrigatorio} 
+        placeholder={placeholder}/> 
+    </div>)
+}
+
+export default CampoTexto
---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,35 @@
+import { AiFillCloseCircle, AiFillHeart, AiOutlineHeart  } from 'react-icons/ai';
+import './colaborador.css'
+
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar, aoFavoritar }) => {
+    function favoritar() {
+        aoFavoritar(colaborador.id);
+    }
+
+    const propsfavorito = {
+        size: 25,
+        onClick: favoritar
+    }
+    return (<div className="colaborador">
+        <AiFillCloseCircle 
+            size={25} 
+            className='deletar' 
+            onClick={() => aoDeletar(colaborador.id)} 
+        />
+        <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
+            <img src={colaborador.imagem} alt={colaborador.nome} />
+        </div>
+        <div className="rodape">
+            <h4>{colaborador.nome}</h4>
+            <h5>{colaborador.cargo}</h5>
+            <div className='favoritar'>
+                {colaborador.favorito 
+                    ? <AiFillHeart {...propsfavorito} color='#FF0000' /> 
+                    : <AiOutlineHeart {...propsfavorito}  />
+                }
+            </div>
+        </div>
+    </div>)
+}
+
+export default Colaborador
